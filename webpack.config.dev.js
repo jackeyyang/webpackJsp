4f79a6eecd8c9ac8d3dbbbf0f25d0736b1bbf7de
jackyyang <jackey088@hotmail.com>
2018-03-17 23:28:15 +0800

    webpack config

---
+++ webpack.config.dev.js
@@ -0,0 +1,12 @@
+/*
+* @Author: jacky.yang
+* @Date:   2018-03-16 17:06:42
+* @Last Modified by:   jacky.yang
+* @Last Modified time: 2018-03-16 17:14:28
+*/
+
+'use strict'
+const webpack = require('webpack');
+// extract-text-webpack-plugin
+// 该插件的主要是为了抽离css样式,防止将样式打包在js中引起页面样式加载错乱的现象
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
