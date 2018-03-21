aa56cfa5727f01dd55c647a7add7122b9a5fc935
jackyyang <jackey088@hotmail.com>
2018-03-21 18:20:14 +0800

    project fold

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,7 +2,7 @@
 * @Author: jacky.yang
 * @Date:   2018-03-16 17:06:42
 * @Last Modified by:   jacky.yang
-* @Last Modified time: 2018-03-16 17:14:28
+* @Last Modified time: 2018-03-21 17:57:40
 */
 
 'use strict'
@@ -14,12 +14,16 @@ const rimraf = require('rimraf'); // 删除文件
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const TemplatePlugin = require('./webpack/templatePlugin'); // 模版生成工具
 const config = require('./webpack/config');
-const glob = require('glob');
-const yamlFrontMatter = require('yaml-front-matter');
+const glob = require('glob'); // 允许你使用 *等符号,写一个glob规则,匹配对应规则的文件
+const yamlFrontMatter = require('yaml-front-matter'); // 使用一段 YAML 语法的头部文本来标记源文件
 const myConfig = require('./webpack/myConfig');
-const loader = require('./webpack/loader');
+const loader = require('./webpack/loader'); // 各种loader设置文件
 
-rimraf.sync(config.OUTPUTPATH_DEV);
+rimraf.sync(config.OUTPUTPATH_DEV); // 清空开发输出的文件夹
+
+// 获取得模板文件夹下的所有模板文件
+const pageFiles = glob.sync('src/projects/**/v/**/*');
+console.log('pageFiles'+ pageFiles);
 
 module.exports = function(argv) {
 	argv = argv || {};
@@ -27,8 +31,8 @@ module.exports = function(argv) {
 	return {
 		entry: './src/scripts/utils/common.js',
 		output: {
-			path: config.OUTPUTPATH_DEV,
-			filename: 'scripts/[name].js',			
+			path: path.join(myConfig.JSP_DEV_PATH),
+			filename: 'scripts/[name].js',
 		},
 		module: {
 			rules: loader()
--- webpack/myConfig.js
+++ webpack/myConfig.js
@@ -1,2 +1,3 @@
-exports.JSP_DEV_PATH = '/usr/local/apache-tomcat-8.0.45/webapps';
+// exports.JSP_DEV_PATH = '/usr/local/apache-tomcat-8.0.45/webapps';
+exports.JSP_DEV_PATH = 'F:\\apache-tomcat-9.0.0.M26\\webapps\\';
 exports.ECLIPSE = true;
